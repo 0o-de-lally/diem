195fe1b1c3d20b7294787965db2004b90fd3c40f
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-09-27 17:26:55 +0000

    set coin name dynamically on compile

--- types/src/utility_coin.rs
+++ types/src/utility_coin.rs
@@ -8,11 +8,27 @@ use move_core_types::{
 };
 use once_cell::sync::Lazy;
 
+use diem_enum_conversion_derive::name_coin;
+
+// NOTE: this expands into:
+
+name_coin!{}
+
 pub static DIEM_COIN_TYPE: Lazy<TypeTag> = Lazy::new(|| {
+    // let n = name_coin!().to_owned();
+    // let  n = name_coin!();
+    // println!("{}", &n);
+    // let u = ident_str!(COIN_MODULE);
     TypeTag::Struct(Box::new(StructTag {
         address: AccountAddress::ONE,
-        module: ident_str!("gas_coin").to_owned(), //////// 0L //////// a number of API endpoint (e.g. simulate_gas) will check for the coin resource and are looking for a specific name, so we're changing this to the generic coin name
-        name: ident_str!("GasCoin").to_owned(), //////// 0L ////////
+        module: ident_str!(COIN_MODULE).to_owned(), //////// 0L //////// a number of API endpoint (e.g. simulate_gas) will check for the coin resource and are looking for a specific name, so we're changing this to the generic coin name
+        name: ident_str!(COIN_NAME).to_owned(), //////// 0L ////////
         type_params: vec![],
     }))
 });
+
+
+#[test]
+fn test_env_coin () {
+  dbg!(&DIEM_COIN_TYPE.clone());
+}
