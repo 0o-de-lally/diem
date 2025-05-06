bd84e01694cf9ebddff3db52d57a7274d90ec969
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-05-06 14:44:09 -0400

    revert to original with expected keyword

--- api/src/runtime.rs
+++ api/src/runtime.rs
@@ -168,10 +168,10 @@ pub fn attach_poem_to_runtime(
             // To allow browsers to use cookies (for cookie-based sticky
             // routing in the LB) we must enable this:
             // https://stackoverflow.com/a/24689738/3846032
-            // .allow_credentials(true)
+            .allow_credentials(true)
             .allow_methods(vec![Method::GET, Method::POST])
             .allow_headers(vec![
-                // header::HeaderName::from_static(X_DIEM_CLIENT),
+                header::HeaderName::from_static("x-aptos-client"),
                 header::CONTENT_TYPE,
                 header::ACCEPT,
             ]);
